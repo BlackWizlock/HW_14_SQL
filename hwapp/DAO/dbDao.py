0785b9533e36aa2c0d7da9fa4223e4a514351845
BlackWizlock <!JQkSr5a>
2022-06-22 07:19:31 +0300

    Full refactoring

--- hwapp/DAO/dbDao.py
+++ hwapp/DAO/dbDao.py
@@ -1,5 +1,4 @@
 import sqlite3
-from flask import jsonify
 
 
 class Database:
@@ -7,94 +6,97 @@ class Database:
         self.database = database
         self.table_name = table_name
 
-    def _database_connection(self, query_string: str):
+    def _database_connection(self):
+        """ Подключаем БД SQL, активируем row_factory """
         with sqlite3.connect(self.database) as con:
             con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            result = cur.execute(query_string)
-            data_flow = result.fetchall()
-        return data_flow
+            cursor = con.cursor()
+        return cursor
 
     def search_title(self, title: str):
-        output = self._database_connection(
-                f"""SELECT 
-                        title, 
-                        country, 
-                        release_year, 
-                        listed_in, 
-                        description 
-                    FROM {self.table_name}
-                    WHERE title LIKE '%{title}%'
-                """)
+        """ Метод класса по поиску названия фильма """
+        cursor = self._database_connection()
+        sqlite_query = f"""
+            SELECT title, country, release_year, listed_in, description 
+            FROM {self.table_name}
+            WHERE title LIKE :substring
+            """
+        cursor.execute(sqlite_query, {"substring": f"%{title}%"})
+        db_results = cursor.fetchall()
         result = []
-        for row in output:
+        for row in db_results:
             result.append(
                     {
                             'title'       : row['title'],
                             'country'     : row['country'],
                             'release_year': row['release_year'],
-                            'genre'   : row['listed_in'],
+                            'genre'       : row['listed_in'],
                             'description' : row['description'].strip()
                     }
             )
-        return jsonify(result)
+        return result
 
     def search_year(self, year_first: int, year_second: int):
-        output = self._database_connection(
-                f"""SELECT 
-                        title, 
-                        release_year 
-                    FROM {self.table_name}
-                    WHERE release_year BETWEEN {year_first} AND {year_second}
-                    ORDER BY release_year DESC 
-                    LIMIT 100
-                """)
+        cursor = self._database_connection()
+        sqlite_query = f"""
+                            SELECT title, release_year 
+                            FROM {self.table_name}
+                            WHERE release_year BETWEEN :substring_year_first AND :substring_year_second
+                            ORDER BY release_year DESC 
+                            LIMIT 100
+                        """
+        cursor.execute(sqlite_query, {"substring_year_first" : f"{year_first}",
+                                      "substring_year_second": f"{year_second}"})
+        db_results = cursor.fetchall()
         result = []
-        for row in output:
+        for row in db_results:
             result.append(
                     {
                             'title'       : row['title'],
                             'release_year': row['release_year']
                     }
             )
-        return jsonify(result)
+        return result
 
     def search_rating(self, rating: str):
-        output = self._database_connection(
-                f"""SELECT
-                        title,
-                        rating,
-                        description
+        cursor = self._database_connection()
+        sqlite_query = """
+                    SELECT title, rating, description
                     FROM {self.table_name}
-                    WHERE rating IN {rating};
-                """)
+                    WHERE rating IN :substring_rating
+                """
+        cursor.execute(sqlite_query, {"substring_rating": f"{rating}"})
+        db_results = cursor.fetchall()
         result = []
-        for row in output:
+        for row in db_results:
             result.append(
                     {
                             'title'      : row['title'],
                             'rating'     : row['rating'],
                             'description': row['description'].strip()
                     }
-            )
-        return jsonify(result)
 
-    def search_genre(self, genre: str):
-        output = self._database_connection(
-                f"""SELECT
-                                title,
-                                description
-                            FROM {self.table_name}
-                            WHERE listed_in LIKE '%{genre}%'
-                            ORDER BY release_year DESC
-                            LIMIT 10;
-                        """)
-        result = []
-        for row in output:
-            result.append(
-                    {
-                            'title'      : row['title'],
-                            'description': row['description'].strip()
-                    }
             )
-        return jsonify(result)
+        return result
+
+
+def search_genre(self, genre: str):
+    cursor = self._database_connection()
+    sqlite_query = f"""
+                        SELECT title, description
+                        FROM {self.table_name}
+                        WHERE listed_in LIKE :substring_genre
+                        ORDER BY release_year DESC
+                        LIMIT 10;
+                    """
+    cursor.execute(sqlite_query, {"substring_genre": f"%{genre}%"})
+    db_results = cursor.fetchall()
+    result = []
+    for row in db_results:
+        result.append(
+                {
+                        'title'      : row['title'],
+                        'description': row['description'].strip()
+                }
+        )
+    return result
--- hwapp/hwapp.py
+++ hwapp/hwapp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify
 from .DAO.dbDao import Database
 
 DB = Database(database="database/netflix.db")
@@ -13,24 +13,31 @@ def index():
 
 @bp.route('/movie/<string:title>')
 def title_page(title: str):
-    return DB.search_title(title=title)
+    db_result = DB.search_title(title=title)
+    return jsonify(db_result)
 
 
 @bp.route('/movie/<int:year_first>/to/<int:year_second>')
 def year_page(year_first: int, year_second: int):
-    return DB.search_year(year_first=year_first, year_second=year_second)
+    db_result = DB.search_year(year_first=year_first, year_second=year_second)
+    return jsonify(db_result)
 
 
 @bp.route('/rating/<string:rating>')
 def rating_page(rating: str):
+    db_result = []
     if rating == "children":
-        return DB.search_rating(rating="('G')")
+        db_result = DB.search_rating(rating="('G')")
     elif rating == "family":
-        return DB.search_rating(rating="('G', 'PG', 'PG-13')")
+        db_result = DB.search_rating(rating="('G', 'PG', 'PG-13')")
     elif rating == "adult":
-        return DB.search_rating(rating="('R', 'NC-17')")
+        db_result = DB.search_rating(rating="('R', 'NC-17')")
+    else:
+        pass
+    return jsonify(db_result)
 
 
 @bp.route('/genre/<string:genre>')
 def genre_page(genre: str):
-    return DB.search_genre(genre=genre)
+    db_result = DB.search_genre(genre=genre)
+    return jsonify(db_result)
